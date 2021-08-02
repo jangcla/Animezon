1e5823c841cacbaf8760cd55bba84d3219bbfa4c
jangcla <jangcla@ymail.com>
2021-08-02 10:57:16 -0700

    user model made

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,39 @@
 class User < ApplicationRecord
+    validates :email, :password_digest, :session_token, presence: true
+    validates :username, uniqueness: true
+    validates :pasword, length: {minimum: 6}, allow_nill: true
+    after_initialize :ensure_session_token
+
+    def self.find_by_credentials(email, password)
+        user = User.find_by(email: email)
+
+        if user && user.is_password?(password)
+            return user
+        else
+            return nil
+        end
+    end
+
+    attr_reader :password
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        password_object = BCrypt::Password.new(self.password_digest)
+        password_object.is_password?(password)
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom.urlsafe_base64
+        self.save
+        self.session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom.urlsafe_base64
+    end
 end
+
+
