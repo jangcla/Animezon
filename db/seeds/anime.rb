d898061e3f689f9bb8d6f1a04464de41438f0d6b
jangcla <jangcla@ymail.com>
2021-08-12 10:56:03 -0700

    seed file change

--- db/seeds/anime.rb
+++ db/seeds/anime.rb
@@ -1,33 +1,33 @@
 anime1 = Item.create!(name: "One Piece", type_tag: "anime", description: "The story of Monkey D. Luffy and his adventure to become the pirate king along with his crew of friends.", availability: true, price: 29.99)
-anime1.photo.attach(io: File.open("/Users/johnangcla/Desktop/animezon_imgages/onepiece.jpeg"), filename: 'onepiece.jpeg')
+anime1.photo.attach(io: open("https://animezon-dev.s3.us-west-1.amazonaws.com/eYYGMkbkFEg422BuErTX4x7i"), filename: 'onepiece.jpeg')
 
 anime2 = Item.create!(name: "Bleach", type_tag: "anime", description: "Kurosaki Ichigo has always been able to see ghosts, but one day this ability causes for an unexpected change of his fate.", availability: true, price: 29.99)
-anime2.photo.attach(io: File.open("/Users/johnangcla/Desktop/animezon_imgages/bleach.jpeg"), filename: 'bleach.jpeg"')
+anime2.photo.attach(io: open("https://animezon-dev.s3.us-west-1.amazonaws.com/LoFHCGKDJMeVr2vd92pV7HA9"), filename: 'bleach.jpeg"')
 
 anime3 = Item.create!(name: "Hunter x Hunter", type_tag: "anime", description: "Gon Freecs has always wanted to meet his father, so when the time came he leaves on an adventure to grow and find his father.", availability: true, price: 24.99)
-anime3.photo.attach(io: File.open("/Users/johnangcla/Desktop/animezon_imgages/hunter.jpeg"), filename: 'hunter.jpeg')
+anime3.photo.attach(io: open("https://animezon-dev.s3.us-west-1.amazonaws.com/KNqkorWvGa1vasDu2c6Fd3XS"), filename: 'hunter.jpeg')
 
 anime4 = Item.create!(name: "Naruto", type_tag: "anime", description: "Naruto Uzumaki has been the town fool for so long, discriminated and hated, the story follows the path of the young shinobi who strives to be the Hokage", availability: true, price: 24.99)
-anime4.photo.attach(io: File.open("/Users/johnangcla/Desktop/animezon_imgages/naruto.jpeg"), filename: 'naruto.jpeg')
+anime4.photo.attach(io: open("https://animezon-dev.s3.us-west-1.amazonaws.com/VTMkrckVHmXTEgpUBN7GXRAY"), filename: 'naruto.jpeg')
 
 anime5 = Item.create!(name: "Naruto Shippuden", type_tag: "anime", description: "The continuation of Naruto and a time skip into a couple years of the original anime, see the teen version of Naruto and his path to becoming one of the greatest ninjas who walked the lands.", availability: true, price: 29.99)
-anime5.photo.attach(io: File.open("/Users/johnangcla/Desktop/animezon_imgages/naruto-shipuden.jpeg"), filename: 'naruto-shipuden.jpeg')
+anime5.photo.attach(io: open("https://animezon-dev.s3.us-west-1.amazonaws.com/racPrsuegmRukp7ZYGTMKz4u"), filename: 'naruto-shipuden.jpeg')
 
 anime6 = Item.create!(name: "Jujutsu Kaisen", type_tag: "anime", description: "The twist of fate that leads to Itadori Yuuji becoming the vessel of an old demonic god and the story of how he strives to conrol his fate", availability: true, price: 34.99)
-anime6.photo.attach(io: File.open("/Users/johnangcla/Desktop/animezon_imgages/jujutsu.jpeg"), filename: 'jujutsu.jpeg')
+anime6.photo.attach(io: open("https://animezon-dev.s3.us-west-1.amazonaws.com/FJxbAFmPGk8PNdUjEbTZibNv"), filename: 'jujutsu.jpeg')
 
 anime7 = Item.create!(name: "Air Gear", type_tag: "anime", description: "Air Gear is about the life of Itsuki Minami and his friends. The story follows their use of Air Trecks, an in-universe invention derived from inline skates.", availability: true, price: 29.99)
-anime7.photo.attach(io: File.open("/Users/johnangcla/Desktop/animezon_imgages/air-gear.jpeg"), filename: 'air-gear.jpeg')
+anime7.photo.attach(io: open("https://animezon-dev.s3.us-west-1.amazonaws.com/mBDPtTpXYqVVd5Ahq5Siqdww"), filename: 'air-gear.jpeg')
 
 anime8 = Item.create!(name: "Boku no Hero Academia", type_tag: "anime", description: "The series focuses on a middle school student Izuku Midoriya, who has no superpowers. ... Academies across the globe train their students to learn to fight crime with their powers. Izuku Midoriya, a boy born without any powers, dreams of being able to become a super hero too, but gets bullied for his unrealistic dreams", availability: true, price: 34.99)
-anime8.photo.attach(io: File.open("/Users/johnangcla/Desktop/animezon_imgages/my-hero.jpg"), filename: 'my-hero.jpg')
+anime8.photo.attach(io: open("https://animezon-dev.s3.us-west-1.amazonaws.com/4gjqG2xMBokukx6wKvRqFyxu"), filename: 'my-hero.jpg')
 
 anime9 = Item.create!(name: "Haikyuu", type_tag: "anime", description: "Hinata Shouyou, a short middle school student, gained a sudden love of volleyball after watching a national championship match on TV. Determined to become like the championship's star player, a short boy nicknamed the small giant, Shouyou joined his school's volleyball club.", availability: true, price: 29.99)
-anime9.photo.attach(io: File.open("/Users/johnangcla/Desktop/animezon_imgages/haikyuu.jpeg"), filename: 'haikyuu.jpeg')
+anime9.photo.attach(io: open("https://animezon-dev.s3.us-west-1.amazonaws.com/KXxNuYiNzPygMQ2Jy6mqecLb"), filename: 'haikyuu.jpeg')
 
 anime10 = Item.create!(name: "Kuroko no Basuke (Kuroko's Basketball)", type_tag: "anime", description: "A legendary 6th player. An up-and-coming power player, Taiga Kagami, is just back from America. When he comes to Seirin High School, he meets the super-ordinary boy, Tetsuya Kuroko. ... Kuroko makes a pact with Kagami to defeat the other members of the Miracle Generation, who have all played basketball at other schools.", availability: true, price: 19.99)
-anime10.photo.attach(io: File.open("/Users/johnangcla/Desktop/animezon_imgages/kuroko.jpg"), filename: 'kuroko.jpg')
+anime10.photo.attach(io: open("https://animezon-dev.s3.us-west-1.amazonaws.com/G7ER8E6NZH8LtgsCtb7PUxNu"), filename: 'kuroko.jpg')
 
 anime11 = Item.create!(name: "Jojo's Bizarre Adventure", type_tag: "anime", description: "The story of the Joestar family, who are possessed with intense psychic strength, and the adventures each member encounters throughout their lives. Chronicles the struggles of the cursed Joestar bloodline against the forces of evil.", availability: true, price: 24.99)
-anime11.photo.attach(io: File.open("/Users/johnangcla/Desktop/animezon_imgages/jojo.png"), filename: 'jojo.png')
+anime11.photo.attach(io: open("https://animezon-dev.s3.us-west-1.amazonaws.com/v4KWoVWuSxrraCnQsY2v49Db"), filename: 'jojo.png')
 
--- db/seeds/merch.rb
+++ db/seeds/merch.rb
@@ -1,24 +1,24 @@
 merch1 = Item.create!(name: "Gear 4th Monkey D. Luffy Snakeman version", type_tag: "merch", description: "A full anime figure replica of Monkey D. Luffy in his 4th gear transformation.", availability: true, price: 39.99)
-merch1.photo.attach(io: File.open("/Users/johnangcla/Desktop/animezon_imgages/g4.jpeg"), filename: 'g4.jpeg')
+merch1.photo.attach(io: open("https://animezon-dev.s3.us-west-1.amazonaws.com/wTjV3WqP5qkQnDzXhQWp3pNb"), filename: 'g4.jpeg')
 
 merch2 = Item.create!(name: "Demon Slayer: Tanjiro's Hanafuda Earings", type_tag: "merch", description: "Replica metal earings, copying the exact look of the one Tanjiro wears in Demon Slayer", availability: true, price: 19.99)
-merch2.photo.attach(io: File.open("/Users/johnangcla/Desktop/animezon_imgages/ds-merch.jpeg"), filename: 'ds-merch.jpeg')
+merch2.photo.attach(io: open("https://animezon-dev.s3.us-west-1.amazonaws.com/Ug25rRT3rXsjRE9aH574nseU"), filename: 'ds-merch.jpeg')
 
 merch3 = Item.create!(name: "Demon Slayer Poster ft. Zenitsu in thunder god form", type_tag: "merch", description: "A 42 by 30 inch poster of Zenitsu in his thunder god stance", availability: true, price: 14.99)
-merch3.photo.attach(io: File.open("/Users/johnangcla/Desktop/animezon_imgages/ds-poster-merch.jpeg"), filename: 'ds-poster-merch.jpeg')
+merch3.photo.attach(io: open("https://animezon-dev.s3.us-west-1.amazonaws.com/z8YGgbixniQDKLRQSnb7LWUm"), filename: 'ds-poster-merch.jpeg')
 
 merch4 = Item.create!(name: "My Hero Aademia Black Graphic Tee", type_tag: "merch", description: "Only selling Large size, making the whole tee fit to be loose and baggy", availability: true, price: 24.99)
-merch4.photo.attach(io: File.open("/Users/johnangcla/Desktop/animezon_imgages/hero-merch.jpeg"), filename: 'hero-merch.jpeg')
+merch4.photo.attach(io: open("https://animezon-dev.s3.us-west-1.amazonaws.com/1BgKX5UQmLRtTEDo4er9Y45t"), filename: 'hero-merch.jpeg')
 
 merch5 = Item.create!(name: "Jujutsu Kaisen character key chains", type_tag: "merch", description: "Jujutsu characters to take on the go as key chains!!!", availability: true, price: 9.99)
-merch5.photo.attach(io: File.open("/Users/johnangcla/Desktop/animezon_imgages/jujutsu-merch.webp"), filename: 'jujutsu-merch.webp')
+merch5.photo.attach(io: open("https://animezon-dev.s3.us-west-1.amazonaws.com/oWxgVaKGPsxhJJ63MsxeFTgf"), filename: 'jujutsu-merch.webp')
 
 merch6 = Item.create!(name: "Jujutsu Kaisen: Itadori Yuuji Phone Case (pack of 2)", type_tag: "merch", description: "A vivid photo of Itaduri Yuuji on a phone case for you to take around. Comes in a pack of two and allows for you to switch between the two whenever needed.", availability: true, price: 14.99)
-merch6.photo.attach(io: File.open("/Users/johnangcla/Desktop/animezon_imgages/jujutsu-phone-merch.jpeg"), filename: 'jujutsu-phone-merch.jpeg')
+merch6.photo.attach(io: open("https://animezon-dev.s3.us-west-1.amazonaws.com/wkn1iQ1nNBQyegL3RY5AfAYx"), filename: 'jujutsu-phone-merch.jpeg')
 
 merch7 = Item.create!(name: "Naruto and Sasuke Anime Figure Set of Two (Asura Gods Edition)", type_tag: "merch", description: "A collective set featuring both Sasuke and Naruto in battle form refering to the two japanese Gods of destruciton", availability: true, price: 99.99)
-merch7.photo.attach(io: File.open("/Users/johnangcla/Desktop/animezon_imgages/naruto-figure-merch.webp"), filename: 'naruto-figure-merch.webp')
+merch7.photo.attach(io: open("https://animezon-dev.s3.us-west-1.amazonaws.com/JQyUd2RNFTLYMNSLuo5BHhvm"), filename: 'naruto-figure-merch.webp')
 
 merch8 = Item.create!(name: "Tokyo Ghoul: One Eyed Ghoul Gaming Mousepad", type_tag: "merch", description: "A wide area gaming mousepad featuring Kaneki Ken in his Ghoul form as the main design of the gaming mouse pad.", availability: true, price: 12.99)
-merch8.photo.attach(io: File.open("/Users/johnangcla/Desktop/animezon_imgages/tokyo-mouse-pad.jpeg"), filename: 'tokyo-mouse-pad.jpeg')
+merch8.photo.attach(io: open("https://animezon-dev.s3.us-west-1.amazonaws.com/c8R9rsjLU2CdsFZkJHgegcui"), filename: 'tokyo-mouse-pad.jpeg')
 
