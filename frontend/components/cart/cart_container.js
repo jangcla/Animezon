a529cff99d0a25ec8eb34df263b9a60d501a74a1
jangcla <jangcla@ymail.com>
2021-08-27 00:21:49 -0700

    last changes for the night

--- frontend/components/cart/cart_container.js
+++ frontend/components/cart/cart_container.js
@@ -4,13 +4,17 @@ import { deleteCart, updateCart } from '../../actions/cart_actions'
 import Cart from './cart'
 
 const mSTP = state => {
-    const cartItems = Object.values(state.entities.cart)
+    const cartItems = Object.values(state.entities.carts)
 
+    let mine = cartItems.filter(cartItem => cartItem.user_id === state.session.id)
 
+    return {
+        mine: mine
+    }
 }
 
 const mDTP = dispatch => {
-
+    return {fetchCarts: () => dispatch(fetchCarts())}
 }
 
 export default connect(mSTP, mDTP)(Cart);
