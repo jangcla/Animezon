920be5744f8f4645a458d7904338512898125881
jangcla <jangcla@ymail.com>
2021-09-15 17:30:06 -0700

    made the item add preview pull the relevant data

---
+++ frontend/components/cart/ItemAdded.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+
+class ItemAdd extends React.Component {
+    render() {
+        return (
+            <div id='item-add-preview'>
+                <div id="item-preview-photo">
+                    <big id='green-cart'>✓</big> 
+                    <img src={this.props.addItem.photoUrl} id='added-photo'/>
+                    <big id='green-cart'>Added to Cart</big>
+                </div>
+
+                <div id="item-preview-text">
+                    <h3>Item has been succesfully added to cart</h3>
+
+                    <h4><big>Cart subtotal</big><small>({this.props.mine.length} items)</small></h4>
+                </div>
+
+            </div>
+        )
+    }
+}
+
+export default ItemAdd;
--- frontend/components/cart/cart_item_add.jsx
+++ frontend/components/cart/cart_item_add.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Navbar from '../nav/navbar';
+import ItemAdd from './ItemAdded';
 
 class ItemAdded extends React.Component {
+    componentDidMount() {
+        this.props.fetchCarts()
+    }
+
     render() {
-        return (
+        return this.props.last ? 
+        (
             <div>
                 <Navbar />
                 <div id="main-cart-comp">
@@ -14,25 +20,33 @@ class ItemAdded extends React.Component {
                             <div id="cart-ad">
                                 <h3>Try Out Animezon Premium</h3>
                             </div>
+                            
 
                         </div>
 
                         <div id="cart-box">
+                            {
+                                this.props.last.map(addedItem => <ItemAdd addItem={addedItem} key={addedItem.id} mine={this.props.mine}/>)
+                            }
                            <h1>Item has been succesfully added to your cart.</h1>
+                                
                         </div>
                     </div>
 
                     <div id="cart-right-div">
                         <div id="cart-item-all-checkout">
                             <p>Continue to shop or go to cart?</p>
-                            <button>Go to Cart</button>
-                            <button>Continue Browsing</button>
+                            <Link to='/cart'>Go to Cart</Link>
+                            <Link to='/'>Continue Browsing</Link>
+
                         </div>
 
                     </div>
                 </div>
             </div>
         )
+        :
+        null;
     }
 }
 
