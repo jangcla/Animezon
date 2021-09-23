e6dd80c2b1e9c0e6d6a75877fe5de428714005e2
jangcla <jangcla@ymail.com>
2021-09-23 14:24:25 -0700

    initiated the components for the review mvp

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -2,6 +2,7 @@ import { combineReducers } from "redux";
 import cartsReducer from "./carts_reducer";
 import itemsReducer from "./items_reducer";
 import itemReducer from "./item_reducer";
+import reviewsReducer from "./reviews_reducer";
 import usersReducer from "./users_reducer";
 
 
@@ -9,7 +10,8 @@ const entitiesReducer = combineReducers({
     users: usersReducer,
     items: itemsReducer,
     item: itemReducer,
-    carts: cartsReducer
+    carts: cartsReducer,
+    reviews: reviewsReducer
 })
 
 export default entitiesReducer;
