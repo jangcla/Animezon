addbc123b02302ee518be1d377e42aa4e9b5f968
jangcla <jangcla@ymail.com>
2021-09-24 23:56:50 -0700

    made a fix on the review form

--- frontend/components/review/review_form_container.js
+++ frontend/components/review/review_form_container.js
@@ -4,6 +4,6 @@ import { createReview, deleteReview } from "../../actions/review_actions";
 import ReviewForm from './review_form';
 
 const mSTP = state => {
-    
+    const item = Object.values(state.entities.items)
 }
 
