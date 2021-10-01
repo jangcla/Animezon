3808af6892a5562448a49f6ee11d10fc9c7e0277
jangcla <jangcla@ymail.com>
2021-09-30 21:03:55 -0700

    made the folder and files for the profile components

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -2,6 +2,7 @@ import { combineReducers } from "redux";
 import cartsReducer from "./carts_reducer";
 import itemsReducer from "./items_reducer";
 import itemReducer from "./item_reducer";
+import profilesReducer from "./profiles_reducer";
 import reviewsReducer from "./reviews_reducer";
 import usersReducer from "./users_reducer";
 
@@ -11,7 +12,8 @@ const entitiesReducer = combineReducers({
     items: itemsReducer,
     item: itemReducer,
     carts: cartsReducer,
-    reviews: reviewsReducer
+    reviews: reviewsReducer,
+    profiles: profilesReducer
 })
 
 export default entitiesReducer;
