3979e3283fd6ab891af0ca235268de977c81ff71
jangcla <jangcla@ymail.com>
2021-09-30 20:44:07 -0700

    made and finished the api util for the profile mvp

---
+++ frontend/util/profile_api_util.js
@@ -0,0 +1,36 @@
+export const createProfile = profile => (
+    $.ajax({
+        method: 'POST', 
+        url: `api/profiles`,
+        data: { profile }
+    })
+)
+
+export const fetchProfile = (profileId) => (
+    $.ajax({
+        method: 'GET',
+        url: `/api/profiles/${profileId}`
+    })
+)
+
+export const fetchProfiles = () => (
+    $.ajax({
+        method: 'GET',
+        url: `/api/profiles`
+    })
+)
+
+export const updateProfile = (profile) => (
+    $.ajax({
+        method: 'PATCH',
+        url: `/api/profiles/${profile.id}`,
+        data: { profile }
+    })
+)
+
+export const deleteProfile = profileId => (
+    $.ajax({
+        method: `DELETE`,
+        url: `api/profiles/${profileId}`
+    })
+)
