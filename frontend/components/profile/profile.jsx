67b31284d85e4a7998f88b46b67967989f5c0363
jangcla <jangcla@ymail.com>
2021-10-01 23:10:18 -0700

    pushing container updates and mispelling bug fixes

---
+++ frontend/components/profile/profile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Navbar from '../nav/navbar'
+
+class Profile extends React.Component {
+    render() {
+        return null;
+    }
+}
+
+export default Profile;
--- frontend/components/profile/profile_container.js
+++ frontend/components/profile/profile_container.js
@@ -1,7 +1,7 @@
 import { connect } from "react-redux";
 import { createProfile, editProfile } from "../../actions/profile_actions";
 
-import Profile './profile'
+import Profile from "./profile";
 
 const mSTP = state => {
     return {
