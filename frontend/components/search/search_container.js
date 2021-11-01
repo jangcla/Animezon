74bfeb81fb762d186c57dcd82a7fc9be51b297f4
jangcla <jangcla@ymail.com>
2021-10-31 23:44:41 -0700

    pushing up the weekend fixes on the search component

--- frontend/components/search/search_container.js
+++ frontend/components/search/search_container.js
@@ -1,1 +1,15 @@
-import 
+import { connect } from 'react-redux';
+
+const mSTP = state => {
+    return {
+
+    }
+}
+
+const mDTP = dispatch => {
+    return {
+        
+    }
+}
+
+export default connect(mSTP, mDTP)(Search);
