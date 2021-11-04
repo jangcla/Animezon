48c9e1011d014af8edf5faaa63b0ea9e98cfd562
jangcla <jangcla@ymail.com>
2021-11-03 23:17:17 -0700

    search container mispelling fix

--- frontend/components/search/search_container.js
+++ frontend/components/search/search_container.js
@@ -2,14 +2,16 @@ import { connect } from 'react-redux';
 import { fetchItems } from '../../actions/item_actions';
 
 const mSTP = state => {
+    const items = Object.values(state.entities.items)
     return {
-
+        items: items
     }
 }
 
 const mDTP = dispatch => {
     return {
-        fetchItems: () => dispatch(fetchItems())
+        fetchItems: () => dispatch(fetchItems()),
+
     }
 }
 
