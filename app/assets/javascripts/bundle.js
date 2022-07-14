0ea3e6358043e0538c3134cfcc1204353ec599b5
jangcla <jangcla@ymail.com>
2022-07-13 20:44:19 -0700

    added in all the new changes to the info features object as well as update the css for the info photo to accomodate the new demo gifs

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -12734,15 +12734,21 @@ var InfoPage = function InfoPage(_ref) {
   (0,react__WEBPACK_IMPORTED_MODULE_0__.useEffect)(function () {
     window.scrollTo(0, 0);
   });
-  var infoPhotos = {
-    splash: "https://github.com/jangcla/Animezon/blob/info/photos/info-photos/info-logo.png?raw=true",
-    slider: "https://github.com/jangcla/Animezon/blob/info/photos/info-photos/auto-slider-demo.gif?raw=true",
-    cart: "https://github.com/jangcla/Animezon/blob/info/photos/info-photos/cart-demo.gif?raw=true",
-    search: "https://github.com/jangcla/Animezon/blob/info/photos/info-photos/search-demo.gif?raw=true",
-    checkout: "https://github.com/jangcla/Animezon/blob/info/photos/info-photos/checkout-demo.gif?raw=true",
-    account: "https://github.com/jangcla/Animezon/blob/info/photos/info-photos/create-account-demo.gif?raw=true",
-    profile: "https://github.com/jangcla/Animezon/blob/info/photos/info-photos/profile-demo.gif?raw=true",
-    review: "https://github.com/jangcla/Animezon/blob/info/photos/info-photos/review-demo.gif?raw=true"
+
+  var updateFeature = function updateFeature(e) {
+    var target = e.currentTarget.value;
+    setFeature(target);
+  };
+
+  var infoFeatures = {
+    splash: ["https://github.com/jangcla/Animezon/blob/info/photos/info-photos/info-logo.png?raw=true", "Welcome to the features informational page of animezon. To understand the useable features of the website, either explore on your own or select one of the feature buttons above."],
+    slider: ["https://github.com/jangcla/Animezon/blob/info/photos/info-photos/auto-slider-demo.gif?raw=true", "On the home page of the website, we can see an automatic photo slider that features an on hover effect. When hovering on the banner, the user is presented with a preview of the product video. Once the banner is clicked, the user is then redirected on to the proper product view page."],
+    cart: ["https://github.com/jangcla/Animezon/blob/info/photos/info-photos/cart-demo.gif?raw=true", "The cart function is used to add, edit and delete possible item purchases onto the cart. With this function, we are able to simulate and display the cart function that Amazon has to offer it's users."],
+    search: ["https://github.com/jangcla/Animezon/blob/info/photos/info-photos/search-demo.gif?raw=true", "With the help of React hooks, Animezon was able to emulate the search bar function that Amazon has to offer. The search function can search either using title names or the different types of items whether it be anime, manga, or merchendise"],
+    checkout: ["https://github.com/jangcla/Animezon/blob/info/photos/info-photos/checkout-demo.gif?raw=true", "The checkout functional component is the following continuation of the cart function. Where the cart function emulates the adding of items to the cart, the checkout function creates new items that the user has purchased over time."],
+    account: ["https://github.com/jangcla/Animezon/blob/info/photos/info-photos/create-account-demo.gif?raw=true", "Animezon allows users to search and view items on the website, but in order to write reviews, add items to cart, purchase items, and even customize one's profile, an account is first needed. To make an account find the accounts and links button on the navbar and signup or login to an account"],
+    profile: ["https://github.com/jangcla/Animezon/blob/info/photos/info-photos/profile-demo.gif?raw=true", "The creation of the user account gives any animezon user a default profile page. But if the user wishes to add another layer personaliztion, one can create a profile page in which other users can see their reviews as well as personalized profile page"],
+    review: ["https://github.com/jangcla/Animezon/blob/info/photos/info-photos/review-demo.gif?raw=true", "As with any e-commerce website the user is able to leave behind reccomendations to other users with the review function. Emulating amazon's reviews system, the user once logged in is able to leave behind a rating ang comment on the product of choice for other users to view either on the product view page or the user profile page"]
   };
   var animezonInfo = /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
     id: "info-about-animezon"
@@ -12778,6 +12784,45 @@ var InfoPage = function InfoPage(_ref) {
   }, "Welcome to Animezon's terms of use information page. To clarify and remind those on the website, Animezon is not a real e-commerce website and no actual purchasing and handling of products are available on the website. This website is simply a fullstack clone of Amazon for learning technologies such as react, javascript, api's, styling, and backend databases."), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
     id: "about-use-of-animezon"
   }, "When interacting with the website, please remember that other people have access to this website and I ask you not to input information you don't want on the internet to see. So please refrain from saving, inputing, and adding sensitive information on the website."));
+  var featuresInfo = /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+    id: "info-about-features"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+    id: "select-a-feature"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("button", {
+    id: "feature-select",
+    onClick: updateFeature,
+    value: "splash"
+  }, "Animezon"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("button", {
+    id: "feature-select",
+    onClick: updateFeature,
+    value: "account"
+  }, "account"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("button", {
+    id: "feature-select",
+    onClick: updateFeature,
+    value: "profile"
+  }, "profile"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("button", {
+    id: "feature-select",
+    onClick: updateFeature,
+    value: "slider"
+  }, "slider"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("button", {
+    id: "feature-select",
+    onClick: updateFeature,
+    value: "cart"
+  }, "cart"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("button", {
+    id: "feature-select",
+    onClick: updateFeature,
+    value: "checkout"
+  }, "checkout"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("button", {
+    id: "feature-select",
+    onClick: updateFeature,
+    value: "search"
+  }, "search"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("button", {
+    id: "feature-select",
+    onClick: updateFeature,
+    value: "review"
+  }, "review")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+    id: "feature-selected-inform"
+  }, infoFeatures[feature][1]));
   return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_nav_navbar__WEBPACK_IMPORTED_MODULE_1__.default, null), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
     id: "info-page"
   }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
@@ -12785,13 +12830,13 @@ var InfoPage = function InfoPage(_ref) {
   }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
     id: "info-photo-container"
   }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("img", {
-    src: infoPhotos[feature],
+    src: infoFeatures[feature][0],
     id: "info-photo"
   })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
     id: "info-text"
   }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h1", {
     id: "info-text-header"
-  }, "Learn about ", infoWord), infoWord === "Animezon" ? animezonInfo : null, infoWord === "Terms of Use" ? termsInfo : null))));
+  }, "Learn about ", infoWord), infoWord === "Animezon" ? animezonInfo : null, infoWord === "Terms of Use" ? termsInfo : null, infoWord === "Functions and Features" ? featuresInfo : null))));
 };
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (InfoPage);
--- frontend/components/info/info_page.jsx
+++ frontend/components/info/info_page.jsx
@@ -3,22 +3,35 @@ import Navbar from "../nav/navbar";
 import { Link } from "react-router-dom";
 
 const InfoPage = ({infoWord}) => {
-
     const [feature, setFeature] = useState("splash");
 
     useEffect(() => {
         window.scrollTo(0, 0);
     })
 
-    const infoPhotos = {
-        splash: "https://github.com/jangcla/Animezon/blob/info/photos/info-photos/info-logo.png?raw=true",
-        slider: "https://github.com/jangcla/Animezon/blob/info/photos/info-photos/auto-slider-demo.gif?raw=true",
-        cart: "https://github.com/jangcla/Animezon/blob/info/photos/info-photos/cart-demo.gif?raw=true",
-        search: "https://github.com/jangcla/Animezon/blob/info/photos/info-photos/search-demo.gif?raw=true",
-        checkout: "https://github.com/jangcla/Animezon/blob/info/photos/info-photos/checkout-demo.gif?raw=true",
-        account: "https://github.com/jangcla/Animezon/blob/info/photos/info-photos/create-account-demo.gif?raw=true",
-        profile: "https://github.com/jangcla/Animezon/blob/info/photos/info-photos/profile-demo.gif?raw=true",
-        review: "https://github.com/jangcla/Animezon/blob/info/photos/info-photos/review-demo.gif?raw=true"
+    const updateFeature = (e) => {
+        const target = e.currentTarget.value;
+
+        setFeature(target);
+    }
+
+    const infoFeatures = {
+        splash: ["https://github.com/jangcla/Animezon/blob/info/photos/info-photos/info-logo.png?raw=true", 
+            "Welcome to the features informational page of animezon. To understand the useable features of the website, either explore on your own or select one of the feature buttons above."],
+        slider: ["https://github.com/jangcla/Animezon/blob/info/photos/info-photos/auto-slider-demo.gif?raw=true", 
+            "On the home page of the website, we can see an automatic photo slider that features an on hover effect. When hovering on the banner, the user is presented with a preview of the product video. Once the banner is clicked, the user is then redirected on to the proper product view page."],
+        cart: ["https://github.com/jangcla/Animezon/blob/info/photos/info-photos/cart-demo.gif?raw=true",
+            "The cart function is used to add, edit and delete possible item purchases onto the cart. With this function, we are able to simulate and display the cart function that Amazon has to offer it's users."],
+        search: ["https://github.com/jangcla/Animezon/blob/info/photos/info-photos/search-demo.gif?raw=true",
+            "With the help of React hooks, Animezon was able to emulate the search bar function that Amazon has to offer. The search function can search either using title names or the different types of items whether it be anime, manga, or merchendise"],
+        checkout: ["https://github.com/jangcla/Animezon/blob/info/photos/info-photos/checkout-demo.gif?raw=true",
+            "The checkout functional component is the following continuation of the cart function. Where the cart function emulates the adding of items to the cart, the checkout function creates new items that the user has purchased over time."],
+        account: ["https://github.com/jangcla/Animezon/blob/info/photos/info-photos/create-account-demo.gif?raw=true",
+            "Animezon allows users to search and view items on the website, but in order to write reviews, add items to cart, purchase items, and even customize one's profile, an account is first needed. To make an account find the accounts and links button on the navbar and signup or login to an account"],
+        profile: ["https://github.com/jangcla/Animezon/blob/info/photos/info-photos/profile-demo.gif?raw=true",
+            "The creation of the user account gives any animezon user a default profile page. But if the user wishes to add another layer personaliztion, one can create a profile page in which other users can see their reviews as well as personalized profile page"],
+        review: ["https://github.com/jangcla/Animezon/blob/info/photos/info-photos/review-demo.gif?raw=true", 
+            "As with any e-commerce website the user is able to leave behind reccomendations to other users with the review function. Emulating amazon's reviews system, the user once logged in is able to leave behind a rating ang comment on the product of choice for other users to view either on the product view page or the user profile page"]
     }
 
     const animezonInfo = (
@@ -73,7 +86,40 @@ const InfoPage = ({infoWord}) => {
         </div>
     )
 
+    const featuresInfo = (
+        <div id="info-about-features">
+            <div id="select-a-feature">
+                <button id="feature-select" onClick={updateFeature} value="splash">
+                    Animezon
+                </button>
+                <button id="feature-select" onClick={updateFeature} value="account">
+                    account
+                </button>
+                <button id="feature-select" onClick={updateFeature} value="profile">
+                    profile
+                </button>
+                <button id="feature-select" onClick={updateFeature} value="slider">
+                    slider
+                </button>
+                <button id="feature-select" onClick={updateFeature} value="cart">
+                    cart
+                </button>
+                <button id="feature-select" onClick={updateFeature} value="checkout">
+                    checkout
+                </button>
+                <button id="feature-select" onClick={updateFeature} value="search">
+                    search
+                </button>
+                <button id="feature-select" onClick={updateFeature} value="review">
+                    review
+                </button>
+            </div>
 
+            <div id="feature-selected-inform">
+                {infoFeatures[feature][1]}
+            </div>
+        </div>
+    )
 
 
     return (
@@ -82,7 +128,7 @@ const InfoPage = ({infoWord}) => {
             <div id="info-page">
                 <div id="info-main-container">
                     <div id="info-photo-container">
-                        <img src={infoPhotos[feature]} id="info-photo"/>
+                        <img src={infoFeatures[feature][0]} id="info-photo"/>
                     </div>
 
                     <div id="info-text">
@@ -100,6 +146,13 @@ const InfoPage = ({infoWord}) => {
                         {
                             infoWord === "Terms of Use" ? 
                                 termsInfo
+                                :
+                                null
+                        }
+
+                        {
+                            infoWord === "Functions and Features" ?
+                                featuresInfo
                                 :
                                 null
                         }
